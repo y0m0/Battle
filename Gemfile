f6f8e03b42bf4f327410783696bedb4e82d493fb
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-25 12:33:40 +0100

    Added sign_in_and_play method and refactored tests

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+
+ruby '2.4.0'
+
 gem "rspec"
 gem "sinatra"
 gem "capybara"
+gem 'simplecov', require: false, group: :test
+gem 'simplecov-console', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,16 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'features/web_helpers'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 Capybara.app = Battle
 
