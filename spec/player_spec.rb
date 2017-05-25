b2b2bb013d808d0ab05ebc2d6591f6b719df5ba3
y0m0 <lixi.daniele@gmail.com>
2017-05-25 16:02:05 +0100

    Now store player 1 and 2 inside Global variables "REALLY BAD!!!" instead of as string in the session.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,9 @@ describe Player do
   subject(:player) { Player.new(name) }
   let(:name) { 'Andrew' }
 
-  it 'has a name' do
-    expect(player.name).to eq name
+  describe '#name' do
+    it 'has a name' do
+      expect(player.name).to eq name
+    end
   end
 end
