a1497960d52655433a85210c63880166acf83aaf
Dom <dom.mcdonnell@gmail.com>
2017-05-26 10:32:15 +0100

    -Created new instance of Game and refactored methods and files to pass existing tests" -Now Game also takes two players upon initalization.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,13 @@
 class Game
+attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
 
   def attack(player)
     player.reduce_hp_when_attacked
   end
-  
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,19 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+
+  it 'should start with two players' do
+    expect(game.player_1).to eq player_1
+    expect(game.player_2).to eq player_2
+  end
 
   describe '#attack' do
     it 'should attack the other player' do
-      expect(player).to receive(:reduce_hp_when_attacked)
-      game.attack(player)
+      expect(player_1).to receive(:reduce_hp_when_attacked)
+      game.attack(player_1)
     end
   end
 end
